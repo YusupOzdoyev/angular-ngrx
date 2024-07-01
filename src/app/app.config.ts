15c17b58e0d0fe1712467e1fa619c96a3554f54a
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2024-07-01 13:15:54 +0300

    add invironments and effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,14 @@ import { routes } from './app.routes';
 import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { authFeatureKey, authReducer } from './auth/store/reducer';
+import { provideHttpClient } from '@angular/common/http';
+import { provideEffects } from '@ngrx/effects';
+import * as authEffects from './auth/store/effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideEffects(authEffects),
+    provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore(),
--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from "@angular/core";
 import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Store } from "@ngrx/store";
-import { register } from "../../store/actions";
+import { authActions } from "../../store/actions";
 import { RegisterRequestInterface } from "../../types/registerRequest.interface";
 import { RouterLink } from "@angular/router";
-import { AuthStateInterface } from "../../types/authState.interface";
 import { AsyncPipe } from "@angular/common";
 import { selectIsSubmitting } from "../../store/reducer";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-register',
@@ -17,7 +17,8 @@ import { selectIsSubmitting } from "../../store/reducer";
 
 export class RegisterComponent {
   private fb = inject(FormBuilder);
-  private store = inject(Store<{ auth: AuthStateInterface }>);
+  private store = inject(Store);
+  private authService = inject(AuthService);
 
   public isSubmitting$ = this.store.select(selectIsSubmitting);
 
@@ -32,6 +33,6 @@ export class RegisterComponent {
     const request: RegisterRequestInterface = {
       user: this.form.getRawValue(),
     };
-    this.store.dispatch(register({ request }));
+    this.store.dispatch(authActions.register({ request }));
   };
 };
